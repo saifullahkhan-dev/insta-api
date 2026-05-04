fee13e23df75aa7a93a9b8f3b3ff4703a41506dc
saifullahkhan-dev <saifullahkhan@outlook.in>
2026-05-04 19:35:59 +0530

    Update post.js

--- api/post.js
+++ api/post.js
@@ -1,3 +1,12 @@
 export default async function handler(req, res) {
-  res.status(200).json({ message: "Post API working ✅" });
+  if (req.method === "POST") {
+    const data = req.body;
+
+    return res.status(200).json({
+      message: "POST received ✅",
+      data: data,
+    });
+  }
+
+  res.status(200).json({ message: "Use POST request" });
 }
